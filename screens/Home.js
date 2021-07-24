9c7b01adedae9b13adeb1b47527cf4ba72d37c7f
Ravindu Senal Fernando <ravinduf@rocketmail.com>
2021-07-24 16:26:47 +0530

    added animated flatlist with a  image background and added play now btn

--- screens/Home.js
+++ screens/Home.js
@@ -15,6 +15,8 @@ import { dummyData, COLORS, SIZES, FONTS, icons, images } from '../constants'
 
 const Home = ({ navigation }) => {
 
+  const newSeasonScrollX = React.useRef(new Animated.Value(0)).current;
+
   const renderHeader = () => {
     return (
       <View
@@ -67,6 +69,116 @@ const Home = ({ navigation }) => {
       </View>
     )
   }
+
+  const renderNewSeasonSection = () => {
+    return (
+      <Animated.FlatList 
+        horizontal
+        pagingEnabled
+        snapToAlignment
+        snapToInterval={SIZES.width}
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={16}
+        decelerationRate={0}
+        contentContainerStyle={{
+          marginTop: SIZES.radius
+        }}
+        data={dummyData.newSeason}
+        keyExtractor={item => `${item.id}`}
+        onScroll={Animated.event([
+          { nativeEvent: { contentOffset: { x: newSeasonScrollX }}}
+        ], { useNativeDriver: false})}
+
+        renderItem={({ item, index }) => {
+          return (
+            <TouchableWithoutFeedback
+              onPress={() => navigation.navigate("MovieDetail", { selectedMovie: item })}
+            >
+              <View
+                style={{ 
+                  width: SIZES.width,
+                  alignItems: 'center', 
+                  justifyContent: 'center',
+                }}
+              >
+                <ImageBackground
+                  source={item.thumbnail}
+                  resizeMode="contain"
+                  style={{
+                    // width: SIZES.width * 0.65,
+                    // height: SIZES.height * 0.65,
+                    width: "100%",
+                    height: SIZES.height * 0.70,
+                    justifyContent: 'flex-end',
+                  }}
+                  imageStyle={{
+                    borderRadius: 30
+                  }}
+                >
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      height: 60,
+                      width: "100%",
+                      marginBottom: SIZES.radius,
+                      paddingHorizontal: SIZES.radius,
+                      // backgroundColor: COLORS.primary,
+                    }}
+                  >
+                    {/* Play Now */}
+                      <View
+                        style={{
+                          flex: 1, 
+                          marginTop: 15,
+                          flexDirection: 'row',
+                          alignItems: 'center', 
+                          justifyContent: 'flex-start'
+                        }}
+                      >
+                        <View
+                          style={{
+                            alignItems: 'center', 
+                            justifyContent: 'center',
+                            width: 40,
+                            height: 40,
+                            borderRadius: 40,
+                            marginLeft: 40,
+                            backgroundColor: COLORS.transparentWhite,
+                          }}
+                        >
+                          <Image
+                            source={icons.play}
+                            resizeMode="contain"
+                            style={{
+                              width: 15,
+                              height: 15,
+                              tintColor: COLORS.white,
+                            }}
+                          >
+
+                          </Image>
+                        </View>
+                          <Text 
+                            style={{
+                              marginLeft: SIZES.base,
+                              color: COLORS.white,
+                              ...FONTS.h3
+                            }}
+                          >
+                            Play Now
+                          </Text>
+                      </View>
+                    {/* Still watching */}
+                  </View>
+
+                </ImageBackground>
+              </View>
+            </TouchableWithoutFeedback>
+          )
+        }}
+      />
+    )
+  }
   return (
     <View
       style={{ 
@@ -75,6 +187,15 @@ const Home = ({ navigation }) => {
       }}
     >
       {renderHeader()}
+
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 100,
+          // height: SIZES.height * 0.50,
+        }}
+      >
+        {renderNewSeasonSection()}
+      </ScrollView>
     </View>
   )
 }
