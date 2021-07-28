3cef2a20055c6719fe66e6c58e3b21ab81902b5f
Ravindu Senal Fernando <ravinduf@rocketmail.com>
2021-07-28 19:52:32 +0530

    added header section and basic styling for movie detials page

--- screens/MovieDetail.js
+++ screens/MovieDetail.js
@@ -1,15 +1,166 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-    View,
-    Text
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Platform,
 } from 'react-native';
 
-const MovieDetail = () => {
+import LinearGradient from 'react-native-linear-gradient';
+
+import { ProgressBar } from '../components';
+import { COLORS , icons, SIZES, FONTS } from '../constants'
+
+const MovieDetail = ({ navigation, route }) => {
+  
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    let { selectedMovie } = route.params;
+    setSelectedMovie(selectedMovie);
+  })
+
+  const renderHeaderBar = () => {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>MovieDetail</Text>
-        </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginTop: Platform.OS === 'ios' ? 40 : 20,
+          paddingHorizontal: SIZES.padding
+        }}
+      >
+        {/* BACk */}
+        <TouchableOpacity
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: 50,
+            height: 50,
+            borderRadius: 20,
+            backgroundColor: COLORS.transparentBlack
+          }}
+          onPress={() => navigation.goBack()}
+        >
+          <Image 
+            source={icons.left_arrow}
+            style={{
+              width: 20,
+              height: 20,
+              tintColor: COLORS.white
+            }}
+          />
+        </TouchableOpacity>
+
+        {/* SHARE */}
+        <TouchableOpacity
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: 50,
+            height: 50,
+            borderRadius: 20,
+            backgroundColor: COLORS.transparentBlack
+          }}
+          onPress={() => console.log("Share")}
+        >
+          <Image 
+            source={icons.upload}
+            style={{
+              width: 20,
+              height: 20,
+              tintColor: COLORS.white
+            }}
+          />
+        </TouchableOpacity>
+      </View>
     )
+  }
+  const renderHeaderSection = () => {
+    return (
+      <ImageBackground
+        source={selectedMovie?.details?.image}
+        resizeMode="cover"
+        style={{
+          width: '100%',
+          height: SIZES.height < 700 ? SIZES.height * 0.6 : SIZES.height * 0.7,
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
+          {renderHeaderBar()}
+
+          <View 
+            style={{
+              flex: 1,
+              justifyContent: 'flex-end'
+            }}
+          >
+            <LinearGradient
+              start={{ x: 0, y: 0 }}
+              end={{ x:0, y: 1}}
+              colors={['transparent', '#000']}
+              style={{
+                width: '100%',
+                height: 140,
+                border: '1px solid white',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+              }}
+            >
+              {/* Season */}
+              <Text
+                style={{
+                  color: COLORS.white,
+                  ...FONTS.body4
+                }}
+              >
+                {selectedMovie?.details.season}                
+              </Text>
+
+              {/* Name */}
+              <Text
+                style={{
+                  color: COLORS.white,
+                  marginTop: SIZES.base,
+                  ...FONTS.h1
+                }}
+              >
+                {selectedMovie?.name}                
+              </Text>
+
+            </LinearGradient>
+
+          </View>
+        </View>
+      </ImageBackground>
+    )
+  }
+
+  
+
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        flex: 1,
+        backgroundColor: COLORS.black,
+      }}
+      style={{ backgroundColor: COLORS.black }}
+    >
+      {/* Header */}
+      {renderHeaderSection()}
+    
+      
+    </ScrollView>
+  )
 }
 
 export default MovieDetail;
